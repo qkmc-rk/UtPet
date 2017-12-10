1412005b8469ca34925f0151f29e4791bfa07bd2
mrruan <314445437@qq.com>
2017-12-10 21:13:16 +0800

    sql

--- src/main/webapp/WEB-INF/sql/YTPet.sql
+++ src/main/webapp/WEB-INF/sql/YTPet.sql
@@ -1,103 +1,88 @@
 /*
-	youta pets db design.
+Date: 2017-12-10
 */
 
-/*
-	t_admin:id,name,account,password,remarks
-	
-*/
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists t_admin;
-create table t_admin(
-	id int primary key auto_increment not null,
-	name varchar(12) UNIQUE ,
-	account varchar(32) not null,
-	password varchar(32) not null
-);
+-- ----------------------------
+-- Table structure for t_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `t_admin`;
+CREATE TABLE `t_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(12) DEFAULT NULL,
+  `account` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4;
 
-/*
-	t_custom_prim:the primary info about user.
+-- ----------------------------
+-- Table structure for t_custom_blc
+-- ----------------------------
+DROP TABLE IF EXISTS `t_custom_blc`;
+CREATE TABLE `t_custom_blc` (
+  `blcid` int(11) NOT NULL AUTO_INCREMENT,
+  `balance` double DEFAULT NULL,
+  `f_customid` int(11) DEFAULT NULL,
+  `updtime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`blcid`),
+  UNIQUE KEY `f_customid` (`f_customid`),
+  CONSTRAINT `t_custom_blc_ibfk_1` FOREIGN KEY (`f_customid`) REFERENCES `t_custom_prim` (`customid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
 
-*/
+-- ----------------------------
+-- Table structure for t_custom_chrginfo
+-- ----------------------------
+DROP TABLE IF EXISTS `t_custom_chrginfo`;
+CREATE TABLE `t_custom_chrginfo` (
+  `chargeid` int(11) NOT NULL AUTO_INCREMENT,
+  `f_customid` int(11) DEFAULT NULL,
+  `chargetime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `amount` double DEFAULT NULL,
+  PRIMARY KEY (`chargeid`),
+  KEY `t_custom_chrginfo_ibfk_1` (`f_customid`),
+  CONSTRAINT `t_custom_chrginfo_ibfk_1` FOREIGN KEY (`f_customid`) REFERENCES `t_custom_prim` (`customid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 
-drop table if exists t_custom_prim;
-create table t_custom_prim(
-	customid int primary key auto_increment not null,
-	customname varchar(12),
-	customphone varchar(32),
-	crttime timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+-- ----------------------------
+-- Table structure for t_custom_pet
+-- ----------------------------
+DROP TABLE IF EXISTS `t_custom_pet`;
+CREATE TABLE `t_custom_pet` (
+  `petid` int(11) NOT NULL AUTO_INCREMENT,
+  `f_customid` int(11) DEFAULT NULL,
+  `pettype` enum('very big','big','small','very small') NOT NULL,
+  `petname` varchar(32) DEFAULT NULL,
+  `petimage` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`petid`),
+  UNIQUE KEY `f_customid` (`f_customid`),
+  CONSTRAINT `t_custom_pet_ibfk_1` FOREIGN KEY (`f_customid`) REFERENCES `t_custom_prim` (`customid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
 
+-- ----------------------------
+-- Table structure for t_custom_prim
+-- ----------------------------
+DROP TABLE IF EXISTS `t_custom_prim`;
+CREATE TABLE `t_custom_prim` (
+  `customid` int(11) NOT NULL AUTO_INCREMENT,
+  `customname` varchar(12) DEFAULT NULL,
+  `customphone` varchar(32) DEFAULT NULL,
+  `crttime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`customid`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
 
-/*
-	t_custom_blc:the money that custom has.(yu e)
-
-*/
-
-
-drop table if exists t_custom_blc;
-create table t_custom_blc(
-	blcid int primary key auto_increment not null,
-	balance double,
-	f_customid int unique,
-	updtime timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	
-	foreign key (f_customid) references t_custom_prim(customid)
-);
-
-/*
-	this is recording that custom usually charge record.
-
-*/
-
-drop table if exists t_custom_chrginfo;
-create table t_custom_chrginfo(
-	chargeid int primary key auto_increment not null,
-	f_customid int,
-	chargetime timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	amount double,
-	
-	foreign key (f_customid) references t_custom_prim(customid)
-);
-
-
-/*
-	this is the pet info about custom
-
-*/
-drop table if exists t_custom_pet;
-create table t_custom_pet(
-	petid int primary key auto_increment not null,
-	f_customid int unique,
-	pettype enum('very big','big','small','very small') NOT NULL,
-	petname varchar(32),
-	petimage varchar(32),
-	
-	foreign key (f_customid) references t_custom_prim(customid)
-);
-
-/*
-	custom purchase history
-
-*/
-
-drop table if exists t_custom_record;
-create table t_custom_record(
-	recordid int primary key auto_increment not null,
-	f_customid int,
-	paytime timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	paywhat varchar(64),
-	amount double,
-	remark varchar(64),
-	
-	foreign key (f_customid) references t_custom_prim(customid)
-);
-
-
-
-
-
-
-
-
-
+-- ----------------------------
+-- Table structure for t_custom_record
+-- ----------------------------
+DROP TABLE IF EXISTS `t_custom_record`;
+CREATE TABLE `t_custom_record` (
+  `recordid` int(11) NOT NULL AUTO_INCREMENT,
+  `f_customid` int(11) DEFAULT NULL,
+  `paytime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `paywhat` varchar(64) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `remark` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`recordid`),
+  KEY `t_custom_record_ibfk_1` (`f_customid`),
+  CONSTRAINT `t_custom_record_ibfk_1` FOREIGN KEY (`f_customid`) REFERENCES `t_custom_prim` (`customid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
